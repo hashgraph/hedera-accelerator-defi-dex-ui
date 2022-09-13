c6dca1f3fa3433b7c5074c02f6a8a5c94665418c
Wayne Sun <wayne.sun@lab49.com>
2022-09-13 12:27:05 -0400

    chore: cleanup commented code

--- src/HederaOpenDEX/pages/Pool/reducers/actionTypes.ts
+++ src/HederaOpenDEX/pages/Pool/reducers/actionTypes.ts
@@ -2,9 +2,6 @@ export enum ActionType {
   UPDATE_INPUT_TOKEN = "UPDATE_INPUT_TOKEN",
   UPDATE_OUTPUT_TOKEN = "UPDATE_OUTPUT_TOKEN",
   SWITCH_INPUT_AND_OUTPUT_TOKEN = "SWITCH_INPUT_AND_OUTPUT_TOKEN",
-  // GET_POOL_LIQUIDITY_STARTED = "GET_POOL_LIQUIDITY_STARTED",
-  // GET_POOL_LIQUIDITY_SUCCESSFUL = "GET_POOL_LIQUIDITY_SUCCESSFUL",
-  // GET_POOL_LIQUIDITY_FAILED = "GET_POOL_LIQUIDITY_FAILED",
 }
 
 interface UpdateInputToken {
@@ -23,26 +20,4 @@ interface SwitchTokens {
   type: ActionType.SWITCH_INPUT_AND_OUTPUT_TOKEN;
 }
 
-// interface GetPoolLiquidityStarted {
-//   type: ActionType.GET_POOL_LIQUIDITY_STARTED;
-// }
-
-// interface GetPoolLiquiditySuccessful {
-//   type: ActionType.GET_POOL_LIQUIDITY_SUCCESSFUL;
-//   poolBalances: {
-//     [symbol: string]: {
-//       amount: number;
-//       address: string;
-//     }
-//   }
-// }
-
-// interface GetPoolLiquidityFailed {
-//   type: ActionType.GET_POOL_LIQUIDITY_FAILED;
-//   errorMessage: string;
-// }
-
 export type PoolActions = UpdateInputToken | UpdateOutputToken | SwitchTokens;
-// | GetPoolLiquidityStarted
-// | GetPoolLiquiditySuccessful
-// | GetPoolLiquidityFailed;
