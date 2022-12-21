59d3a44118b7abe539f25b3246e2fd8260632851
Roshan Singh Bisht <roshan.bisht@lab49.com>
2022-12-21 15:16:33 +0530

    Removed Unnecessarystyles
    Signed-off-by: Roshan Bisht roshan.bisht@lab49.com
    Signed-off-by: Roshan Singh Bisht <roshan.bisht@lab49.com>

--- src/dex-ui-components/base/Inputs/FileUploader.tsx
+++ src/dex-ui-components/base/Inputs/FileUploader.tsx
@@ -1,14 +1,22 @@
 import { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
 import { Card, Text } from "@chakra-ui/react";
-import { customFileUploaderStyles } from "./styles/FileUploaderStyles";
-
+import { Color } from "../../../dex-ui-components";
 interface File extends Blob {
   readonly lastModified: number;
   readonly name: string;
   readonly webkitRelativePath: string;
 }
 
+const defaultFileUploaderStyles = {
+  bg: "white",
+  width: "100%",
+  textAlign: "center",
+  justifyContent: "center",
+  alignSelf: "center",
+  border: "1px #31A9BD dashed",
+  height: "102Px",
+};
 interface FileUploaderProps {
   title: string;
   subTitle: string;
@@ -50,19 +58,14 @@ function FileUploader(props: FileUploaderProps) {
   });
 
   return (
-    <Card
-      {...getRootProps()}
-      variant="outline"
-      sx={customFileUploaderStyles}
-      style={{ justifyContent: "center", ...fileUploaderStyles }}
-    >
+    <Card {...getRootProps()} style={{ ...defaultFileUploaderStyles, ...fileUploaderStyles }}>
       <input {...getInputProps()} />
       {isDragActive ? (
-        <Text color={"#31A9BD"}>{onHoverTitle}</Text>
+        <Text color={Color.Blue_03}>{onHoverTitle}</Text>
       ) : (
         <>
-          <Text color={"#31A9BD"}>{title}</Text>
-          <Text color={"#31A9BD"}>{subTitle}</Text>
+          <Text color={Color.Blue_03}>{title}</Text>
+          <Text color={Color.Blue_03}>{subTitle}</Text>
         </>
       )}
     </Card>
--- src/dex-ui-components/base/Inputs/styles/FileUploaderStyles.ts
+++
@@ -1,9 +0,0 @@
-export const customFileUploaderStyles = {
-  bg: "white",
-  width: "100%",
-  textAlign: "center",
-  justify: "center",
-  alignSelf: "center",
-  border: "1px #31A9BD dashed",
-  height: "102Px",
-};
