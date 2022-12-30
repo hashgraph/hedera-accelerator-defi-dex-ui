533a2bfdbd90f7c401477260f73e265e86e515db
Roshan Singh Bisht <roshan.bisht@lab49.com>
2022-12-30 16:18:15 +0530

    Yarn format correct.
    Signed-off-by: Roshan Singh Bisht <roshan.bisht@lab49.com>

--- src/dex-ui/pages/CreateProposal/utils.ts
+++ src/dex-ui/pages/CreateProposal/utils.ts
@@ -1,10 +1,10 @@
 const checkIsValidUrl = (urlString: string) => {
-    try {
-        const url = new URL(urlString);
-        return url.protocol === "http:" || url.protocol === "https:";
-    } catch (_) {
-        return urlString.length === 0 ? true : false;
-    }
+  try {
+    const url = new URL(urlString);
+    return url.protocol === "http:" || url.protocol === "https:";
+  } catch (_) {
+    return urlString.length === 0 ? true : false;
+  }
 };
 
 export { checkIsValidUrl };
