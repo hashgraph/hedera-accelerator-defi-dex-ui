3be2d0b632bb25da78c40618c96166a4eae0bad5
Roshan Singh Bisht <roshan.bisht@lab49.com>
2023-01-31 22:23:19 +0530

    yarn format fix
    Signed-off-by: Roshan Singh Bisht <roshan.bisht@lab49.com>

--- src/dex-ui-components/base/Stepper/Stepper.tsx
+++ src/dex-ui-components/base/Stepper/Stepper.tsx
@@ -1,5 +1,5 @@
 import Stepper, { Step, StepperContent } from "react-material-stepper";
-import 'react-material-stepper/dist/react-stepper.css';
+import "react-material-stepper/dist/react-stepper.css";
 import { Box, Text } from "@chakra-ui/react";
 interface ProposalStates {
   status: string;
@@ -14,15 +14,8 @@ function StepperUI(props: StepperState | undefined) {
   return (
     <Box>
       <Stepper vertical={true} initialStep={1}>
-        <Step
-          stepId={1}
-          data="Step 1 initial state"
-          title="Step One"
-          description="This step is optional"
-        >
-          <StepperContent>
-            Step1 resolved:
-          </StepperContent>
+        <Step stepId={1} data="Step 1 initial state" title="Step One" description="This step is optional">
+          <StepperContent>Step1 resolved:</StepperContent>
           {/* <Text>Step 1</Text> */}
         </Step>
       </Stepper>
