2cafb5379812cb2607815eee673cee009bdf9809
Damiano Melcarne <damiano.melcarne@lab49.com>
2023-02-01 14:25:25 -0500

    feat: Added icons for proposal selection
    Signed-off-by: Damiano Melcarne <damiano.melcarne@lab49.com>

--- src/dex-ui-components/base/Icons/index.ts
+++ src/dex-ui-components/base/Icons/index.ts
@@ -2,3 +2,7 @@ export * from "./SettingsIcon";
 export * from "./SwitchIcon";
 export * from "./ArrowDropDownIcon";
 export * from "./TooltipIcon";
+export * from "./TextIcon";
+export * from "./TrendingUpIcon";
+export * from "./WalletIcon";
+export * from "./TokenIcon";
