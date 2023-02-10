2f48c471686c4857c82c5141e94c2bc62865a0d3
Roshan Singh Bisht <roshan.bisht@lab49.com>
2023-02-10 17:27:26 +0530

    corrected the calculation of quorum and remaining votes using bignumber library

--- src/dex-ui/pages/Governance/formatter.ts
+++ src/dex-ui/pages/Governance/formatter.ts
@@ -2,6 +2,7 @@ import { Proposal } from "../../store/governanceSlice";
 import { formatDuration } from "../../utils";
 import { FormattedProposal } from "./types";
 import { DEX_TOKEN_PRECISION_VALUE } from "../../services";
+import { BigNumber } from "bignumber.js";
 
 /**
  * Coverts proposal data into a format that will be displayed in the UI.
@@ -11,13 +12,16 @@ import { DEX_TOKEN_PRECISION_VALUE } from "../../services";
 export const formatProposal = (proposal: Proposal): FormattedProposal => {
   const { id, contractId, type, title, author, description, status, timeRemaining, state, votes, link, timestamp } =
     proposal;
-  const [yes, no, abstain, quorum, max] = [
+  const [yes, no, abstain, max] = [
     votes.yes?.shiftedBy(-DEX_TOKEN_PRECISION_VALUE).toNumber(),
     votes.no?.shiftedBy(-DEX_TOKEN_PRECISION_VALUE).toNumber(),
     votes.abstain?.shiftedBy(-DEX_TOKEN_PRECISION_VALUE).toNumber(),
-    votes.quorum?.shiftedBy(-DEX_TOKEN_PRECISION_VALUE).toNumber(),
     votes.max?.shiftedBy(-DEX_TOKEN_PRECISION_VALUE).toNumber(),
   ];
+
+  const quorum = votes.quorum?.div(votes.max ?? BigNumber(1)).multipliedBy(BigNumber(100)).toNumber();
+  const totalVotes = votes.yes?.plus(votes.no ?? BigNumber(0)).plus(votes.abstain ?? BigNumber(0));
+  const remaining = votes.max?.minus(totalVotes ?? BigNumber(0)).shiftedBy(-DEX_TOKEN_PRECISION_VALUE).toNumber();
   return {
     id: id.toString(),
     contractId,
@@ -34,8 +38,8 @@ export const formatProposal = (proposal: Proposal): FormattedProposal => {
       yes,
       no,
       abstain,
-      quorum: (Number(quorum) / Number(max)) * 100,
-      remaining: Number(max) - (Number(yes) + Number(no) + Number(abstain)),
+      quorum,
+      remaining,
       max,
     },
   };
