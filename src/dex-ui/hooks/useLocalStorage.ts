f3cee0a41cd3286e7a44f931ab35ff5b670b418f
Roshan Singh Bisht <roshan.bisht@lab49.com>
2023-02-20 12:14:32 +0530

    Code review Feedbacks
    Signed-off-by: Roshan Singh Bisht <roshan.bisht@lab49.com>

--- src/dex-ui/hooks/useLocalStorage.ts
+++ src/dex-ui/hooks/useLocalStorage.ts
@@ -1,17 +1,15 @@
 import { ChangeEvent, useEffect, useState } from "react";
 
-function getStorageValue<T>(key: string, defaultValue: T) {
-  if (typeof window !== "undefined") {
-    const storageValue = localStorage.getItem(key);
-    const localStorageValue = storageValue !== null ? JSON.parse(storageValue) : defaultValue;
-    return localStorageValue;
-  }
-}
-
 export function useLocalStorage<T>(key: string, defaultValue: T) {
   const [value, setValue] = useState(() => {
     return getStorageValue(key, defaultValue);
   });
+
+  function getStorageValue<T>(key: string, defaultValue: T) {
+    const storageValue = localStorage.getItem(key);
+    const localStorageValue = storageValue !== null ? JSON.parse(storageValue) : defaultValue;
+    return localStorageValue;
+  }
 
   function handleChange(event: ChangeEvent<HTMLInputElement>) {
     const inputElement = event?.target as HTMLInputElement;
