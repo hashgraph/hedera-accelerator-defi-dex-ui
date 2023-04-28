17014ee0db1ff704e7ee38c7cf5c442cc012cfb5
Roshan Singh Bisht <roshan.bisht@lab49.com>
2023-04-28 17:11:41 +0530

    removed deploy contract from UI side to remove client dependencies
    Signed-off-by: Roshan Singh Bisht <roshan.bisht@lab49.com>

--- src/dex-ui/hooks/governance/useCreateProposal.tsx
+++ src/dex-ui/hooks/governance/useCreateProposal.tsx
@@ -1,18 +1,15 @@
 import { GovernanceMutations, GovernanceQueries } from "./types";
 import { TransactionResponse } from "@hashgraph/sdk";
-import { HashConnectSigner } from "hashconnect/dist/esm/provider/signer";
 import { useMutation, useQueryClient } from "react-query";
 import { DexService } from "../../services";
 import { CreateProposalType } from "./types";
-import { WalletStore } from "../../store/walletSlice";
+import { useDexContext } from "../useDexContext";
 
 interface CreateNewTokenProposalData {
   title: string;
-  signer: HashConnectSigner;
 }
 
 interface CreateTextProposalData {
-  signer: HashConnectSigner;
   title: string;
   description: string;
   linkToDiscussion: string;
@@ -25,7 +22,6 @@ interface CreateTransferTokenProposalData {
   accountToTransferTo: string;
   tokenToTransfer: string;
   amountToTransfer: number;
-  wallet: WalletStore;
 }
 
 interface CreateContractUpgradeProposalData {
@@ -34,7 +30,6 @@ interface CreateContractUpgradeProposalData {
   linkToDiscussion: string;
   implementationAddress: string;
   proxyAddress: string;
-  signer: HashConnectSigner;
 }
 
 interface CreateProposalData {
@@ -50,11 +45,14 @@ type CreateProposalDataTypes =
 
 export function useCreateProposal() {
   const queryClient = useQueryClient();
+  const { wallet } = useDexContext(({ wallet }) => ({ wallet }));
+  const signer = wallet.getSigner();
+
   return useMutation<TransactionResponse, Error, CreateProposalData, GovernanceMutations.ExecuteProposal>(
     (params: CreateProposalData) => {
       const { type, proposalData } = params;
       if (type === CreateProposalType.ContractUpgrade) {
-        const { linkToDiscussion, title, signer, description, implementationAddress, proxyAddress } =
+        const { linkToDiscussion, title, description, implementationAddress, proxyAddress } =
           proposalData as CreateContractUpgradeProposalData;
         return DexService.sendCreateContractUpgradeProposalTransaction({
           title,
@@ -65,11 +63,11 @@ export function useCreateProposal() {
           linkToDiscussion,
         });
       } else if (type === CreateProposalType.Text) {
-        const { signer, title, description, linkToDiscussion } = proposalData as CreateTextProposalData;
+        const { title, description, linkToDiscussion } = proposalData as CreateTextProposalData;
         return DexService.sendCreateTextProposalTransaction(title, description, linkToDiscussion, signer);
       } else {
         // TODO: In the default case its Token Tranfer
-        const { wallet, title, description, linkToDiscussion, tokenToTransfer, amountToTransfer, accountToTransferTo } =
+        const { title, description, linkToDiscussion, tokenToTransfer, amountToTransfer, accountToTransferTo } =
           proposalData as CreateTransferTokenProposalData;
         const preciseTransferTokenAmount = wallet.getTokenAmountWithPrecision(tokenToTransfer, amountToTransfer);
         return DexService.sendCreateTransferTokenProposalTransaction({
@@ -79,7 +77,7 @@ export function useCreateProposal() {
           tokenToTransfer,
           amountToTransfer: preciseTransferTokenAmount,
           accountToTransferTo,
-          signer: wallet.getSigner(),
+          signer: signer,
         });
       }
     },
--- src/dex-ui/services/HederaService/utils.ts
+++ src/dex-ui/services/HederaService/utils.ts
@@ -1,22 +1,12 @@
-import { ADMIN_ID, ADMIN_KEY, TREASURY_ID, TREASURY_KEY, TOKEN_USER_ID, TOKEN_USER_KEY } from "../constants";
+import { TREASURY_ID, TREASURY_KEY, TOKEN_USER_ID, TOKEN_USER_KEY } from "../constants";
 import { PrivateKey, Client, AccountId, TransactionResponse, TransactionReceipt } from "@hashgraph/sdk";
 import { isNil } from "ramda";
-
-const adminId = AccountId.fromString(ADMIN_ID);
-const adminKey = PrivateKey.fromString(ADMIN_KEY);
 
 const treasuryId = AccountId.fromString(TREASURY_ID);
 const treasuryKey = PrivateKey.fromString(TREASURY_KEY);
 
 const userId = AccountId.fromString(TOKEN_USER_ID);
 const userKey = PrivateKey.fromString(TOKEN_USER_KEY);
-
-const getAdmin = () => {
-  return {
-    adminId,
-    adminKey,
-  };
-};
 
 const getTreasurer = () => {
   return {
@@ -38,10 +28,6 @@ const createClient = (accountId: AccountId, privateKey: PrivateKey): Client => {
   return client;
 };
 
-const createAdminClient = (): Client => {
-  return createClient(adminId, adminKey);
-};
-
 const createTreasuryClient = (): Client => {
   return createClient(treasuryId, treasuryKey);
 };
@@ -59,11 +45,9 @@ const checkTransactionResponseForError = (response: TransactionResponse | Transa
 export {
   client,
   checkTransactionResponseForError,
-  getAdmin,
   getTreasurer,
   getUser,
   createClient,
-  createAdminClient,
   createTreasuryClient,
   createUserClient,
 };
