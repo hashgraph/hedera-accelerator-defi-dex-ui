cfc71a5f09033bcb265c3751b8e69d1e8abc877c
Damiano Melcarne <damiano.melcarne@lab49.com>
2023-10-18 15:07:44 -0400

    feat: Temp remove DAO / DEX routing
    Signed-off-by: Damiano Melcarne <damiano.melcarne@lab49.com>

--- src/index.tsx
+++ src/index.tsx
@@ -6,33 +6,11 @@ import { DEXStoreProvider } from "@dex/context";
 import { initializeServices } from "@dex/services";
 import { DEFAULT_DEX_PROVIDER_PROPS } from "@dex/store";
 
-const DAO_HOSTNAME = "dao";
-const DEX_HOSTNAME = "defi-ui";
-const DEV_ENV = "development";
-/**
- * `shouldRenderDEX = false` will render the DAO app.
- * `shouldRenderDEX = true` will render the DEX app.
- * TODO: Setup feature flag for running DAO or DEX app.
- * */
-const shouldRenderDEX = false;
-
 /** Needed to enable immutable immer updates on Map and Set objects. */
 enableMapSet();
 
 initializeServices().then(() => {
   const container = document.getElementById("root") as HTMLElement;
   const root = createRoot(container);
-  const devHostname = shouldRenderDEX ? DEX_HOSTNAME : DAO_HOSTNAME;
-  const subdomain = process.env.NODE_ENV === DEV_ENV ? devHostname : window.location.hostname.split(".")?.[0];
-
-  function getApp(): React.ReactNode {
-    if (subdomain.includes(DAO_HOSTNAME)) return <DAO />;
-    if (subdomain.includes(DEX_HOSTNAME)) {
-      return <DEX />;
-    }
-    /** TODO: Style this error message component. */
-    return <>Cannot resolve hostname</>;
-  }
-
-  root.render(<DEXStoreProvider {...DEFAULT_DEX_PROVIDER_PROPS}>{getApp()}</DEXStoreProvider>);
+  root.render(<DEXStoreProvider {...DEFAULT_DEX_PROVIDER_PROPS}>{<DAO />}</DEXStoreProvider>);
 });
